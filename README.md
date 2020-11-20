b285feb735e050c42f408bfb70fcac7e85648f98
Aaron <aaronwey@posteo.de>
2020-11-20 12:00:23 +0100

    Better Readme, Verbosity-Setting implemented
    Took 5 minutes

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # pass-testhaveibeenpwned
 
-
 This python script  runs your pass-store [1] against a database of known leaked passwords[2].
-It all happens offline, you may find technical details at [3].
+The critical steps happens offline, you may find technical details at [3]. Your passwords never
+leave your machine.
 
-It is work in progress, but should be usable as is.
+It is work in progress, but should be usable as is. Some (early) versions
+of this software will print your plaintext-passwords to console,
+so please be aware, especially in public spaces.
 
 Simply running main should be sufficient for using this software.
 
--- main.py
+++ main.py
@@ -1,20 +1,36 @@
+import sys
 from pathlib import Path
 
 from src.utils import extract_file_paths, extract_plain_text_passwort_from_pgp_file, check_password
 
 
 def main():
+    print("To enable passwort-printing, run with -v")
+
     gpg_files = extract_file_paths(Path().home().joinpath("./.password-store"))
 
     passwords_found = []
+    password_found = False
     for file_el in gpg_files:
         plain_text_pass_word = extract_plain_text_passwort_from_pgp_file(file_el)
         result = check_password(plain_text_pass_word)
         if result:
             passwords_found.append((file_el.name, plain_text_pass_word))
+        password_found = passwords_found or result
 
-    for el in passwords_found:
-        print(el)
+    if "-v" in sys.argv:
+        print("Verbosity is set, printing the plaintext-passwords")
+
+        for el in passwords_found:
+            print(el)
+        exit(0)
+
+    if password_found:
+        print("Atleast one password has been found")
+        exit(0)
+    else:
+        print("No passwort has been found")
+        exit(0)
 
 
 if __name__ == "__main__":
